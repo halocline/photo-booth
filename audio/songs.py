a4e09f3ebd29b35eeeaf9a1670187e2b0334d31d
Halocline <mdglissmann@gmail.com>
2020-12-15 18:12:12 -0700

    more jingle bells

--- audio/songs.py
+++ audio/songs.py
@@ -6,5 +6,15 @@ def jingleBells():
         'Ew',  # way
         'F', 'F', 'F', 're', 'Fe',  # oh what fun
         'F', 'E', 'E', 'Ee', 'Ee',  # it is to ride
+        'E', 'D', 'D', 'E',  # in a one-horse
+        'Dh', 'Gh',  # open sleigh. Oh!
+        'E', 'E', 'Eh',  # jin-gle bells
+        'E', 'E', 'Eh',  # jin-gle bells
+        'E', 'G', 'C', 're', 'De',  # jin-gle all the way
+        'Ew',  # way
+        'F', 'F', 'F', 'F',  # oh what fun
+        'F', 'E', 'E', 'Ee', 'Ee',  # it is to ride
+        'G', 'G', 'F', 'D',  # in a one-horse open
+        'Cw'  # sleigh
     ]
     return notes
