3a7ad5cf060bf778a9883b0e40240833bdccc91c
Halocline <mdglissmann@gmail.com>
2020-12-15 19:45:37 -0700

    project structure

---
+++ screens/cli_transitions.py
@@ -0,0 +1,25 @@
+def byeScreen():
+    print('      .      ')
+    print('      :      ')
+    print('\'.___/*\___.\'')
+    print('  \* \ / */  ')
+    print('   >--X--<   ')
+    print('  /*_/ \_*\  ')
+    print('.\'   \* /   \'.')
+    print('      :      ')
+    print('      \'      ')
+    print('\n')
+    print('Merry Christmas!' + '\n' + '\n')
+
+
+def welcomeScreen(name):
+    print('\n' + '   *      *   ')
+    print('   _\/  \/_   ')
+    print('    _\/\/_    ')
+    print('_\_\_\/\/_/_/_')
+    print(' / /_/\/\_\ \ ')
+    print('    _/\/\_    ')
+    print('    /\  /\    ')
+    print('   *      *   ')
+    print('Season\'s Greetings!' + '\n' + '\n')
+    print('Welcome to ' + name + '\n' + '\n')
